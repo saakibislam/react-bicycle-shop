74b23b41a825a4623b8a40c44ea3416bdd91662b
saakibislam <islam.sakibul@gmail.com>
2026-04-05 20:25:43 -0400

    PurchaseModal address field generated

--- src/Pages/BicycleDetails/PurchaseModal.js
+++ src/Pages/BicycleDetails/PurchaseModal.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal, Row } from "react-bootstrap";
 import useFirebase from "../hooks/useFirebase";
 
 const PurchaseModal = ({ bicycle, show, handleClose, toggleToast }) => {
@@ -9,7 +9,12 @@ const PurchaseModal = ({ bicycle, show, handleClose, toggleToast }) => {
   const cycleRef = useRef();
   const dateRef = useRef();
   const priceRef = useRef();
-  const addressRef = useRef();
+  const phoneRef = useRef();
+  const streetRef = useRef();
+  const apartmentRef = useRef();
+  const zipRef = useRef();
+  const cityRef = useRef();
+  const countryRef = useRef();
 
   const handleOnSubmit = () => {
     const name = nameRef.current.value;
@@ -17,15 +22,25 @@ const PurchaseModal = ({ bicycle, show, handleClose, toggleToast }) => {
     const cycleName = cycleRef.current.value;
     const purchasedDate = dateRef.current.value;
     const priceValue = priceRef.current.value;
-    const deliverAddress = addressRef.current.value;
+    const phone = phoneRef.current.value;
+    const street = streetRef.current.value;
+    const apartment = apartmentRef.current.value;
+    const zip = zipRef.current.value;
+    const city = cityRef.current.value;
+    const country = countryRef.current.value;
 
     const order = {
-      buyerName: name,
-      buyerEmail: email,
+      name,
+      email,
+      phone,
+      street,
+      apartment,
+      zip,
+      city,
+      country,
       cycleType: cycleName,
       purchasedOn: purchasedDate,
       price: priceValue,
-      address: deliverAddress,
     };
     // console.log(order);
     fetch("http://localhost:5000/api/v2/orders", {
@@ -56,14 +71,14 @@ const PurchaseModal = ({ bicycle, show, handleClose, toggleToast }) => {
       </Modal.Header>
       <Modal.Body>
         <div>
-          <form>
+          <Form>
             <Form.Group
               className="mb-3 text-start"
               controlId="exampleForm.ControlInput1"
             >
               <Form.Label>Name</Form.Label>
               <Form.Control
-                type="email"
+                type="text"
                 defaultValue={user.displayName}
                 ref={nameRef}
                 placeholder="Your Name"
@@ -81,59 +96,107 @@ const PurchaseModal = ({ bicycle, show, handleClose, toggleToast }) => {
                 placeholder="name@example.com"
               />
             </Form.Group>
-            <Form.Group
-              className="mb-3 text-start"
-              controlId="exampleForm.ControlInput1"
-            >
-              <Form.Label>Cycle Type</Form.Label>
+            <Row xs={1} sm={1} md={2} className="g-4">
+              <Form.Group
+                className="mb-3 text-start"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Cycle Type</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={bicycle.name}
+                  ref={cycleRef}
+                  placeholder="Cycle Name"
+                  disabled
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3 text-start"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Purchase Date</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={new Date().toLocaleDateString()}
+                  ref={dateRef}
+                  disabled
+                  placeholder="Purchased Date"
+                />
+              </Form.Group>
+            </Row>
+            <Row xs={1} sm={1} md={2} className="g-4">
+              <Form.Group
+                className="mb-3 text-start"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Price</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={bicycle.price}
+                  ref={priceRef}
+                  disabled
+                  placeholder="Product Price"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 text-start">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  ref={phoneRef}
+                  placeholder="Phone Number"
+                />
+              </Form.Group>
+            </Row>
+            <Form.Group className="mb-3 text-start">
+              <Form.Label>Street</Form.Label>
               <Form.Control
-                type="text"
-                defaultValue={bicycle.name}
-                ref={cycleRef}
-                placeholder="Cycle Name"
-                disabled
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3 text-start"
-              controlId="exampleForm.ControlInput1"
-            >
-              <Form.Label>Purchase Date</Form.Label>
-              <Form.Control
-                type="text"
-                defaultValue={new Date().toLocaleDateString()}
-                ref={dateRef}
-                disabled
-                placeholder="Purchased Date"
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3 text-start"
-              controlId="exampleForm.ControlInput1"
-            >
-              <Form.Label>Price</Form.Label>
-              <Form.Control
-                type="text"
-                defaultValue={bicycle.price}
-                ref={priceRef}
-                disabled
-                placeholder="Product Price"
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3 text-start"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                autoFocus
                 required
                 type="text"
-                ref={addressRef}
-                placeholder="Deliver Address"
+                ref={streetRef}
+                placeholder="Street Address"
               />
             </Form.Group>
-          </form>
+            <Row xs={1} sm={1} md={2} className="g-4">
+              <Form.Group className="mb-3 text-start">
+                <Form.Label>Apartment</Form.Label>
+                <Form.Control
+                  type="text"
+                  ref={apartmentRef}
+                  placeholder="Apartment, suite, etc."
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 text-start">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  ref={cityRef}
+                  placeholder="City"
+                />
+              </Form.Group>
+            </Row>
+            <Row xs={1} sm={1} md={2} className="g-4">
+              <Form.Group className="mb-3 text-start">
+                <Form.Label>ZIP Code</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  ref={zipRef}
+                  placeholder="ZIP Code"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 text-start">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  ref={countryRef}
+                  placeholder="Country"
+                />
+              </Form.Group>
+            </Row>
+          </Form>
         </div>
       </Modal.Body>
       <Modal.Footer>
